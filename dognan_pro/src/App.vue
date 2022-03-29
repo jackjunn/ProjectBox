0ce95b29ca91341736e0bab5278c1a2bc232bb10
JackJunn <214402875@qq.com>
2022-03-29 21:18:58 +0800

    dognan-1

---
+++ dognan_pro/src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <div id="app">
+    <vue-particles
+      color="#dedede"
+      :particleOpacity="0.7"
+      :particlesNumber="80"
+      shapeType="circle"
+      :particleSize="4"
+      linesColor="#dedede"
+      :linesWidth="1"
+      :lineLinked="false"
+      :lineOpacity="0.4"
+      :linesDistance="150"
+      :moveSpeed="3"
+      :hoverEffect="true"
+      hoverMode="grab"
+      :clickEffect="true"
+      clickMode="push"
+      class="login-bg"
+    >
+    </vue-particles>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 0;
+}
+body{
+  margin: 0;
+  background: #2c3e50;
+}
+.login-bg{
+  width: 100%;
+  height: calc(100% - 100px);
+  position: absolute;
+}
+</style>
---
+++ dognan_pro/src/components/HelloWorld.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="hello">
+    <h1>敬请期待。。。</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1,
+h2 {
+  font-weight: normal;
+  color: rgb(34, 168, 40);
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
